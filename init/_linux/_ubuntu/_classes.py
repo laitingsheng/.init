f84a6bfa72d8e5c147efad384f529ce2222290dc
Tinson Lai <laitingsheng@hotmail.com>
2021-09-20 00:06:01 +0800

    Restructure APT operations

--- init/_linux/_ubuntu/_classes.py
+++ init/_linux/_ubuntu/_classes.py
@@ -1,114 +1,290 @@
+import configparser
 import fnmatch
 import math
+import operator
 import os
 import platform
+import re
 import shutil
 import subprocess
 import sys
+import urllib.request
+from abc import ABCMeta
+from multiprocessing import Pool
 from pathlib import Path
-from typing import Any, Dict
-from urllib import request
+from typing import Any, Dict, Iterable, Iterator, List
 
-import apt
-import aptsources.sourceslist
 import lsb_release
+from apt import Cache, Package
+from aptsources.sourceslist import SourcesList
 
-_DIST_INFO = lsb_release.get_distro_information()
-
-
-def _reset_file(file: Path):
-    file.unlink(True)
-    file.touch(0o644, False)
-    os.chown(file, 0, 0)
+_APT_PREFERENCES_DIR = Path("/etc/apt/preferences.d")
+_APT_SOURCES_LIST_DIR = Path("/etc/apt/sources.list.d")
+_APT_TRUSTED_GPG_DIR = Path("/etc/apt/trusted.gpg.d")
 
 
-def _reset_folder(folder: Path):
-    if folder.exists():
-        shutil.rmtree(folder)
-    folder.mkdir(0o755, False, False)
-    os.chown(folder, 0, 0)
+def _exponentials(space: int) -> Iterator[int]:
+    start = 1
+    while True:
+        yield start
+        start *= space
 
 
-class _Variables:
-    __slots__ = "autoremove", "codename", "desktop", "max_download_size", "max_install_size", "prune", "release", "upgrade", "wsl"
-
-    def __init__(self, variables: Dict[str, Any]):
-        self.codename = _DIST_INFO["CODENAME"]
-        self.release = _DIST_INFO["RELEASE"]
-        self.desktop = False
-        self.wsl = True
-        self.prune = True
-        self.autoremove = True
-        self.upgrade = True
-        self.max_download_size = math.inf
-        self.max_install_size = math.inf
-
-        for k, v in variables.items():
-            setattr(self, k, v)
-
-        if self.wsl and not fnmatch.fnmatchcase(platform.release(), "*-microsoft-standard-WSL2"):
-            self.wsl = False
-
-        if self.wsl:
-            self.desktop = False
-
-        if self.max_download_size is None:
-            self.max_download_size = math.inf
-
-        if self.max_install_size is None:
-            self.max_install_size = math.inf
-
-
-class _Entries:
-    __slots__ = "architectures", "components", "dists", "uri"
+class _SimpleData(metaclass=ABCMeta):
+    __slots__ = ()
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
-            setattr(self, k, v)
+            setattr(self, f"_{k}", v)
+
+    def __repr__(self) -> str:
+        attributes = ", ".join(f"{attr}={getattr(self, attr)}" for attr in self._attributes)
+        return f"{self.__class__.__name__}({attributes})"
+
+    @property
+    def _attributes(self) -> Iterable[str]:
+        return self.__slots__
 
 
-class _APTConfig:
-    __slots__ = "_keys", "_packages", "_preferences", "_preferences_d", "_sources", "_sources_list_d", "_trusted_gpg_d", "_variables"
+class _APTKeyFile(_SimpleData):
+    __slots__ = "_name", "_uri"
 
-    def __init__(self, apt_config: Dict[str, Any], variables: _Variables):
-        self._sources_list_d = Path("/etc/apt/sources.list.d")
-        self._preferences_d = Path("/etc/apt/preferences.d")
-        self._trusted_gpg_d = Path("/etc/apt/trusted.gpg.d")
+    def save(self):
+        with urllib.request.urlopen(self._uri) as f:
+            p = subprocess.run(["gpg", "--dearmor"], input=f.read(), capture_output=True)
 
-        self._variables = variables
+        if p.returncode:
+            raise RuntimeError(f"failed to fetch key for {self._name}\n{p.stderr.decode('utf-8')}")
 
-        sources = self._sources = {}
-        keys = self._keys = {}
-        packages = self._packages = set()
+        key_path = _APT_TRUSTED_GPG_DIR / f"{self._name}.gpg"
+        with open(key_path, "wb") as f:
+            f.write(p.stdout)
+        os.chown(key_path, 0, 0)
+        os.chmod(key_path, 0o644)
 
-        for source_name, source_config in apt_config.items():
-            output = source_config.get("output", source_name)
-            entries = sources[output] = sources.get(output, {})
-            entries[source_name] = _Entries(
-                uri=self._normalise_uri(source_config["uri"]),
-                dists=source_config["dists"] if "dists" in source_config else [
-                    f"{self._variables.codename}{suffix}"
-                    for suffix in source_config["dist_suffices"]
-                ] if "dist_suffices" in source_config else [self._variables.codename],
-                components=source_config.get("components", ["main"]),
-                architectures=source_config.get("architectures", [])
-            )
 
-            if "key" in source_config:
-                if source_name in keys:
-                    raise RuntimeError(f"duplicate key for {source_name}")
+class _APTRepoFile(_SimpleData):
+    __slots__ = "_architectures", "_components", "_dists", "_name", "_uri"
+
+    def write_to(self, sources_list: SourcesList):
+        file = _APT_SOURCES_LIST_DIR / f"{self._name}.list"
+        for dist in self._dists:
+            sources_list.add("deb", self._uri, dist, self._components, file=file, architectures=self._architectures)
+
+
+class _WSLConfigFile(_SimpleData):
+    __slots__ = (
+        "_automount_enabled",
+        "_automount_mountFsTab",
+        "_automount_options",
+        "_automount_root",
+        "_network_generateHosts",
+        "_network_generateResolvConf",
+        "_interop_enabled",
+        "_interop_appendWindowsPath",
+        "_user_default",
+        "_wsl2_guiapplications"
+    )
+
+    _DEFAULT_USER = os.environ.get("SUDO_USER", os.environ["USER"])
+
+    def __init__(self, **kwargs):
+        self._automount_enabled = True
+        self._automount_mountFsTab = True
+        self._automount_options = "metadata,umask=0022,fmask=0022,dmask=0022"
+        self._automount_root = "/mnt/"
+        self._network_generateHosts = True
+        self._network_generateResolvConf = True
+        self._interop_enabled = True
+        self._interop_appendWindowsPath = False
+        self._user_default = None
+        self._wsl2_guiapplications = True
+
+        for section_name, section in kwargs.items():
+            for attribute_name, attribute in section.items():
+                setattr(self, f"_{section_name}_{attribute_name}", attribute)
+
+        if self._user_default is None:
+            self._user_default = self._DEFAULT_USER
+
+    def save(self):
+        config = configparser.ConfigParser()
+        for variable in self._attributes:
+            _, section, option = variable.split('_')
+            if not config.has_section(section):
+                config.add_section(section)
+            config.set(section, option, str(getattr(self, variable)))
+
+        wsl_conf_path = Path("/etc/wsl.conf")
+        with open(wsl_conf_path, "w", encoding="utf-8") as f:
+            config.write(f)
+        os.chown(wsl_conf_path, 0, 0)
+        os.chmod(wsl_conf_path, 0o644)
+
+
+class _Initialiser(_SimpleData):
+    __slots__ = (
+        "_apt_keys",
+        "_apt_packages",
+        "_apt_repos",
+        "_var_autoremove",
+        "_var_codename",
+        "_var_desktop",
+        "_var_keep_old_config",
+        "_var_max_download_size",
+        "_var_max_install_size",
+        "_var_release",
+        "_var_upgrade",
+        "_var_wsl",
+        "_wsl_conf"
+    )
+
+    _DIST_INFO = lsb_release.get_distro_information()
+
+    _SIZE_TABLE = {
+        k: v
+        for k, v in zip(
+            ("", "K", "M", "G", "T", "P", "E", "Z", "Y"),
+            _exponentials(1024)
+        )
+    }
+
+    @staticmethod
+    def _reset_files(*args: List[Path]):
+        for file in args:
+            file.unlink(True)
+            file.touch(0o644, False)
+            os.chown(file, 0, 0)
+
+    @staticmethod
+    def _reset_folders(*args: List[Path]):
+        for folder in args:
+            if folder.exists():
+                shutil.rmtree(folder)
+            folder.mkdir(0o755, False, False)
+            os.chown(folder, 0, 0)
+
+    def __init__(self, config: Dict[str, Any]):
+        self._var_codename = self._DIST_INFO["CODENAME"]
+        self._var_release = self._DIST_INFO["RELEASE"]
+        self._var_keep_old_config = False
+        self._var_desktop = False
+        self._var_wsl = True
+        self._var_autoremove = True
+        self._var_upgrade = True
+        self._var_max_download_size = None
+        self._var_max_install_size = None
+
+        # the "variables" field is optional
+        for k, v in config.get("variables", {}).items():
+            setattr(self, f"_var_{k}", v)
+
+        # TODO: add detection of different WSL versions instead of boolean value
+        if self._var_wsl and not fnmatch.fnmatchcase(platform.release(), "*-microsoft-standard-WSL2"):
+            print(f"Disabling WSL on non-WSL Ubuntu installation...", file=sys.stderr)
+            self._var_wsl = False
+
+        # TODO: maybe removed in the future when WSLg releases
+        if self._var_wsl:
+            self._var_desktop = False
+
+        self._normalise_sizes("max_download", "max_install")
+
+        apt_config = config["apt"]
+
+        # parepare for overwriting the config if requested
+        if not self._var_keep_old_config:
+            self._apt_repos = [
+                _APTRepoFile(
+                    name=repo_name,
+                    uri=self._normalise_uri(repo_config["uri"]),
+                    dists=repo_config["dists"] if "dists" in repo_config else [
+                        f"{self._var_codename}{suffix}"
+                        for suffix in repo_config["codename_suffices"]
+                    ] if "codename_suffices" in repo_config else [self._var_codename],
+                    components=repo_config.get("components", ["main"]),
+                    architectures=repo_config.get("architectures", [])
+                )
+                for repo_name, repo_config in apt_config["repos"].items()
+            ]
+            self._apt_keys = [
+                _APTKeyFile(
+                    name=key_name,
+                    uri=self._normalise_uri(key_uri)
+                )
+                for key_name, key_uri in apt_config["keys"].items()
+            ]
+
+        self._apt_packages = apt_config["packages"]
+
+        if self._var_wsl:
+            self._apt_packages.extend(("wsl", "ubuntu-wsl"))
+
+            self._wsl_conf = _WSLConfigFile(**config["wsl"])
+
+    @property
+    def _attributes(self) -> Iterable[str]:
+        for attr in self.__slots__:
+            if attr.startswith("_var_"):
+                yield attr
+
+    def _apt(self):
+        if not self._var_keep_old_config:
+            self._reset_folders(_APT_SOURCES_LIST_DIR, _APT_PREFERENCES_DIR, _APT_TRUSTED_GPG_DIR)
+            self._reset_files(Path("/etc/apt/sources.list"), Path("/etc/apt/preferences"), Path("/etc/apt/trusted.gpg"))
+            Path("/etc/apt/trusted.gpg~").unlink(True)
+
+            sources_list = SourcesList()
+            for repo in self._apt_repos:
+                repo.write_to(sources_list)
+            sources_list.save()
+
+            with Pool() as pool:
+                pool.map(operator.methodcaller("save"), self._apt_keys)
+
+        with Cache() as cache:
+            cache.update()
+
+        # first mark all packages to be edited (install/upgrade)
+        packages = set(self._apt_packages)
+        with Cache() as cache, cache.actiongroup():
+            for package in cache:
+                package_name = package.name
+                if package_name in packages:
+                    package.mark_install(True, True, True)
+                    package.mark_auto(False)
+                    packages.remove(package_name)
                 else:
-                    keys[source_name] = self._normalise_uri(source_config["key"])
+                    package.mark_auto(True)
+            cache.commit()
+        if packages:
+            print("The following missing packages were ignored:", file=sys.stderr)
+            for package in packages:
+                print(f"  * {package}", file=sys.stderr)
 
-            packages.update(source_config.get("packages", []))
+        # remove unused packages if requested
+        with Cache() as cache, cache.actiongroup():
+            for package in cache:
+                if package.is_auto_removable:
+                    if self._var_autoremove:
+                        package.mark_delete(True, True)
+                elif package.is_upgradable:
+                    if self._var_upgrade:
+                        package.mark_upgrade(not package.is_auto_installed)
+            cache.commit()
 
-        if self._variables.wsl:
-            packages.add("ubuntu-wsl")
-            packages.add("wsl")
-
-            if "cuda" in apt_config:
-                sources["nvidia"]["cuda"].uri = "https://mirrors.aliyun.com/nvidia-cuda/wsl-ubuntu/x86_64"
-                keys["cuda"] = "https://mirrors.aliyun.com/nvidia-cuda/wsl-ubuntu/x86_64/7fa2af80.pub"
+    def _normalise_sizes(self, *args: List[str]):
+        for size_key in args:
+            size_name = f"_var_{size_key}_size"
+            size = getattr(self, size_name)
+            if size is None:
+                setattr(self, size_name, math.inf)
+            elif isinstance(size, (int, float)):
+                setattr(self, size_name, size)
+            elif isinstance(size, dict):
+                setattr(self, size_name, size["size"] * self._SIZE_TABLE[size["unit"]])
+            else:
+                raise ValueError(f"Unrecognised size specification for {size_key} ({size})")
 
     def _normalise_uri(self, uri_config) -> str:
         if isinstance(uri_config, str):
@@ -116,92 +292,29 @@ class _APTConfig:
         if isinstance(uri_config, dict):
             format_args = {}
             for k, v in uri_config["format_args"].items():
-                if "value" in v:
-                    format_args[k] = v["value"]
+                if isinstance(v, (str, int, float, bool)):
+                    format_args[k] = v
+                elif isinstance(v, dict):
+                    format_args[k] = getattr(self, f"_substitute_{v.pop('type')}")(**v)
                 else:
-                    variable = getattr(self._variables, v["variable"])
-                    if "method" in v:
-                        func = v["method"]
-                        variable = getattr(variable, func["name"])(*func.get("args", []), **func.get("kwargs", {}))
-                    format_args[k] = variable
+                    raise TypeError(f"unrecognised format_args type ({format_args})")
             return uri_config["template"].format(**format_args)
-        else:
-            raise TypeError(f"{uri_config} is neither a string nor a recognised config")
+        raise TypeError(f"{uri_config} is neither a string nor a recognised config")
+
+    def _size_check(self, package: Package) -> bool:
+        pass
+
+    def _substitute_method(self, name: str, args=[], kwargs={}):
+        return getattr(self, f"_sup_method_{name}")(*args, **kwargs)
+
+    def _substitute_variable(self, name: str):
+        return getattr(self, f"_var_{name}")
+
+    def _sup_method_cuda_uri_folder(self) -> str:
+        return "wsl-ubuntu" if self._var_wsl else f"ubuntu{self._var_release.replace('.', '')}"
 
     def run(self):
-        cache = apt.Cache()
-        cache.update()
+        self._apt()
 
-        # edit current packages
-        with cache, cache.actiongroup():
-            for package in cache:
-                if package.name in self._packages:
-                    package.mark_install(True, True, True)
-                    package.mark_auto(False)
-                    self._packages.remove(package.name)
-                else:
-                    package.mark_auto(True)
-            cache.commit()
-
-        # remove unused packages
-        if self._variables.autoremove:
-            with cache, cache.actiongroup():
-                for package in cache:
-                    if package.is_auto_removable:
-                        package.mark_delete(True, True)
-                cache.commit()
-
-        # perform an upgrade on all installed packages
-        if self._variables.upgrade:
-            with cache, cache.actiongroup():
-                cache.upgrade(True)
-                cache.commit()
-
-    def prune(self):
-        if self._variables.prune:
-            _reset_folder(self._sources_list_d)
-            _reset_folder(self._preferences_d)
-            _reset_folder(self._trusted_gpg_d)
-
-            _reset_file(Path("/etc/apt/sources.list"))
-            _reset_file(Path("/etc/apt/preferences"))
-            _reset_file(Path("/etc/apt/trusted.gpg"))
-            Path("/etc/apt/trusted.gpg~").unlink(True)
-
-    def save_keys(self):
-        for k, v in self._keys.items():
-            with request.urlopen(v) as f:
-                p = subprocess.run(["gpg", "--dearmor"], input=f.read(), capture_output=True)
-
-            if p.returncode:
-                raise RuntimeError(f"failed to fetch key for {k}\n{p.stderr.decode('utf-8')}")
-
-            with open(self._trusted_gpg_d / f"{k}.gpg", "wb") as f:
-                f.write(p.stdout)
-
-    def save_sources(self):
-        sources_list = aptsources.sourceslist.SourcesList()
-        for k, entries in self._sources.items():
-            file = self._sources_list_d / f"{k}.list"
-            for entry in entries.values():
-                for dist in entry.dists:
-                    sources_list.add("deb", entry.uri, dist, entry.components, file=file, architectures=entry.architectures)
-        sources_list.save()
-
-
-class _Initialiser:
-    __slots__ = "_apt", "_variables"
-
-    def __init__(self, config: Dict[str, Any]):
-        self._variables = _Variables(config.get("variables", {}))
-        self._apt = _APTConfig(config["apt"], self._variables)
-
-    def prune(self):
-        self._apt.prune()
-
-    def run_apt(self):
-        self._apt.run()
-
-    def save(self):
-        self._apt.save_sources()
-        self._apt.save_keys()
+        if self._var_wsl:
+            self._wsl_conf.save()
